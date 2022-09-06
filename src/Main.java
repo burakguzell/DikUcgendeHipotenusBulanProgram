3b3c1b56786810e44c8205e7e06fc91265210ac6
burakguzell <102960797+mrputcher@users.noreply.github.com>
2022-09-06 15:10:58 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Değişkenleri tanımlayalım.
+
+        int a,b ;
+        double c ;
+
+        //Kullanıcıdan verilerimizi alalım..
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("1. Kenarı giriniz : ");
+        a = input.nextInt();
+        System.out.print("2. Kenarı giriniz : ");
+        b = input.nextInt();
+
+        c = Math.sqrt((a*a) + (b*b));
+        System.out.println("Hipotenüs : "+ c);
+
+    }
 }
